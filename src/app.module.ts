063bdf7b66691c531bf4befc37d45be38ef8fbe0
kkmiloo <camilo.reyes_granada@uao.edu.co>
2024-06-24 12:28:04 -0500

    add: middleware to see the body

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CommonModule } from './common/common.module';
 import { CustomApiModule } from './custom-api/custom-api.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [UserModule, CommonModule, CustomApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: ':code/:api_name/:id', method: RequestMethod.PUT });
+  }
+}
